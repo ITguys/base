679069afaf492ae82a68752b03ab9ca60a793663
Jio Lee <jypandjio@163.com>
2014-02-11 23:53:16 +0800

    Added init data

---
+++ app/models/app_config.rb
@@ -0,0 +1,7 @@
+class AppConfig < Settingslogic
+  config_file = File.expand_path("../../../config/app_config.yml"， __FILE__)
+  raise "#{config_file} not exists!" unless File.exists?
+  source config_file
+  namespace Rails.env
+  load!
+end
