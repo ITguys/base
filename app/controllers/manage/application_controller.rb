e0f9f06ee12bcede3b926b8d0842f67c90691b57
lijia.tong <wtuyuupe@163.com>
2014-02-21 01:30:10 +0800

    验证用户改写到后吧Manage下

---
+++ app/controllers/manage/application_controller.rb
@@ -0,0 +1,18 @@
+class Manage::ApplicationController < ApplicationController
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  layout 'manage/application'
+  protect_from_forgery with: :exception
+  before_action :require_login, except: [:not_authenticated]
+
+  def index
+    render text: '', layout: true
+  end
+
+  protected
+
+  def not_authenticated
+    redirect_to new_session_path, alert: "Please login first."
+  end
+
+end
